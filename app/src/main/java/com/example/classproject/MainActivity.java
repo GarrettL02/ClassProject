13f5ee5693d66a17181f4791db9fe53947463269
GarrettL02 <garrett0216@gmail.com>
2024-04-24 00:21:33 -0400

    2nd version

--- app/src/main/java/com/example/classproject/MainActivity.java
+++ app/src/main/java/com/example/classproject/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_bars, R.id.navigation_notifications)
+                R.id.navigation_homes, R.id.navigation_bars, R.id.navigation_weather)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
